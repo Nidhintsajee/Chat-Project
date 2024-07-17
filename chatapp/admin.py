c3d63b84c619180b852b9fb7f31f03c33b0084d1
Nidhintsajee <nidhintsajee@gmail.com>
2024-07-17 12:04:55 +0530

    model mirations added

--- chatapp/admin.py
+++ chatapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Interest,ChatMessage
 # Register your models here.
+
+admin.site.register(Interest)
+admin.site.register(ChatMessage)
--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
+
+class Interest(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_interests', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_interests', on_delete=models.CASCADE)
+    message = models.TextField()
+    accepted = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class ChatMessage(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
